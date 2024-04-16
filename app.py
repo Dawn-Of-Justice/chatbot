00e4bb8da55ffb4320ae07d72882f2ed2ef3f19b
SALO SOJA <salosoja@gmail.com>
2024-04-17 01:56:33 +0530

    Updated beautify function
    Updated beautify function to remove the path and return the name of the doc only

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ doc ="files/attention-is-all-you-need-Paper.pdf"
 
 def beutify(name):
 
+    name = name.replace("files/", "")
     name = name.replace(".pdf", "")
     words = name.split("-")
     camel_case = ' '.join(word.capitalize() for word in words[0:])
